82f39fa27d2466a9b8a623e1132dd0018a6a06a3
Alec McGail <amcgail2@gmail.com>
2023-11-26 18:55:58 -0500

    adding the first test

---
+++ simple question.py
@@ -0,0 +1,21 @@
+import openai
+from openai import OpenAI
+
+# set API KEY
+client = OpenAI(api_key = "API_KEY")
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages = [
+        {"role": "system", "content": "You are an advanced AI DM. Make a fun adventure"},
+        {"role": "user", "content": "I am a fighter named Grog. I am looking for a quest"},
+    ],
+    max_tokens=None,
+    temperature=0.9,
+    top_p=1,
+    frequency_penalty=0.0,
+    presence_penalty=0.6,
+    #stop=["\n"],
+)
+
+print(response)
