c8c7b649fc4589783f5e4eda1a3c13f58757e084
bartohanew <thbarrow@outlook.com>
2023-11-27 15:09:26 -0500

    working on putting all the .txt files together

--- toms_code.py
+++ toms_code.py
@@ -3,15 +3,35 @@ from common import *
 with open('characteristics_wizard.txt', 'r') as file:
     characteristics_wizard = file.read()
 
+with open('how_to_dm.txt', 'r') as file:
+    how_to_dm = file.read()
+
+with open('story/adventure_hook.txt', 'r') as file:
+    adventure_hook = file.read()
+
+with open('story/backstory.txt', 'r') as file:
+    backstory = file.read()
+
+with open('story/forgotten_realms.txt', 'r') as file:
+    forgotten_realms = file.read()
+
+with open('story/overview.txt', 'r') as file:
+    overview = file.read()
+
 sys_message = f"""
 {characteristics_wizard}
+{how_to_dm}
+{backstory}
+{adventure_hook}
+{forgotten_realms}
+{overview}
 
 You are to interpret the actions this wizard would take and answer quetions based on the information provided
 """
 
 M = [
     {"role": "system", "content": sys_message}, 
-    {"role": "user", "content": "what does galanodel translate to?"}, # player says
+    {"role": "user", "content": "i want to start my adventure"}, # player says
 ]
 
 r1 = get_response(M)
