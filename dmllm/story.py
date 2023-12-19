4dda1cc391ff5539fb80fd8873ca6751481c368e
bartohanew <thbarrow@outlook.com>
2023-12-19 16:26:44 -0500

    w

---
+++ dmllm/story.py
@@ -0,0 +1,21 @@
+from common import *
+from knowledge import Knowledge
+
+class Story(Knowledge):
+    questions = {
+        {
+  "elementsOfAGreatAdventure": {
+    "credibleThreat": "Generate a credible threat. An adventure needs a threat worthy of the heroes' attention, like a single villain, a villain with lackeys, an assortment of monsters, or an evil organization.",
+    "familiarTropesWithCleverTwists": "Generate familiar tropes with clever twists to be used throughout the adventure. Use familiar elements like dragons, orcs, wizards, but add unique twists to them.",
+    "clearFocusOnThePresent": "Focus on the present situation, what the bad guys are up to, and how the adventurers become involved in the story.",
+    "heroesWhoMatter": "Allow adventurers' actions and decisions to matter and influence the outcome.",
+    "somethingForAllPlayerTypes": "Include a balance of exploration, social interaction, and combat to appeal to all types of players.",
+    "surprises": "Include surprises and unique elements to keep players engaged, but don't overdo it.",
+    "adventureStructure": {
+      "beginning": "Start with an exciting hook to get players interested and provide a clear sense of where the story is headed.",
+      "middle": "Develop the bulk of the story where adventurers make choices that affect the conclusion.",
+      "ending": "Create a strong climax where the outcome depends on the characters' actions and decisions, and leave some story threads hanging for future adventures."
+    }
+  }
+}
+    }
